4e5d23ec0d5f99517927a81821dbcec56eca4575
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2025-04-01 22:47:32 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,10 @@
+def hello_world()      # : is missed
+    print("Hello, World!")
+
+
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+
+def add_numbers(a: b):   # Instead of : , will come
+    return a + b
